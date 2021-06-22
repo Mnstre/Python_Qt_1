bbef0b92155f07e3c858709584c4c1e3f6dea30b
Miguel <gmiguel.503@gmail.com>
2021-06-22 13:59:50 -0500

    qwerty

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-import sys
-from PySide6.QtWidgets import QApplication, QLabel
+# Seacrh Tree in Python
+#
+class Nodo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-app = QApplication(sys.argv)
-label = QLabel("Hello World!")
-label.show()
-app.exec()
+class Vector:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+class Tree:
+    def __init__(self):
+        self.nNodos = None
+
+    def addNodo(self, x, y):
+        nodo = Nodo(x, y)
+    
