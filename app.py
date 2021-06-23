6c468932262ce66e81f6f852fe3fee46df327163
Miguel <gmiguel.503@gmail.com>
2021-06-23 13:55:50 -0500

    qweeer

--- app.py
+++ app.py
@@ -2,4 +2,12 @@
 #       - Mnstre @ June, 2021
 from graphManager import GraphManager
 
-gM = GraphManager()
+if __name__ == '__main__':
+    gM = GraphManager()
+    file = open('graph.txt', 'r')
+    Lines = file.readlines()
+
+    cBlock = 0
+    cLines = 0
+    for line in Lines:
+        print("{:d}. {d}")
