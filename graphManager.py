133d682bb9b0748175ae1e8faaa5e40c5710e6bd
Mnstre <gmiguel.503@gmail.com>
2021-08-16 20:12:25 -0500

    big1

--- graphManager.py
+++ graphManager.py
@@ -2,24 +2,39 @@
 #   - Constructor and functions
 from vertex import VertexFactory
 
+# 3 etapas 
+#   1- nVertex
+#   2- mapVertex
+#   3- nEdges
+#   4- mapEdges
+def decrypter(file1):
+    lista = []        
+    etapa = 0
+    for linea in file:
+    for letra in linea:
+        if letra.isdigit():
+            dato = dato + letra
+        else:
+            lista = lista + [int(dato)]
+            dato = ''
+
 class GraphManager:
     def __init__(self):
         self.__nVertex = None
         self.__m_keyGen = 0
         self.__mapV = None
         self.__mapE = None
-    
+
     def __createVertex(self, vFactory):
         v = vFactory.createFactory(self.__m_keyGen)
-
         self.__m_keyGen +=1
+        return v
     
     def loadGraph(self, file, vFactory, eFactory):
         file1 = open("graph.txt", "r")
-        self.__nVertex = file1[0]
-        if self.__nVertex:
-            for iterator in file1[1:self.__nVertex]:
-                nodo = self.__createVertex(vFactory)
+
+        nVertex, nEdges, mapVertex, mapEdges = decrypter(file1)
+
         x = None
         y = None
         ########
--- test_For.py
+++ test_For.py
@@ -2,6 +2,13 @@
 counter = 0
 file = open("graph.txt", "r")
 
+lista = []
+
 for linea in file:
-    counter += 1
-    espacio = False
+    dato = ""
+    for letra in linea:
+        if letra.isdigit():
+            dato = dato + letra
+        else:
+            lista = lista + [int(dato)]
+            dato = ''
